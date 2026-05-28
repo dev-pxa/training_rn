ea7f81646841a602daee682a3206c951a2af7111
xiaan <1967914901@qq.com>
2026-05-29 00:22:07 +0800

    feat: 新增网络日志功能并添加个人中心原型页面
    1. 安装并引入react-native-network-logger库，启用应用内网络日志功能
    2. 新增企业培训APP个人中心原型HTML页面

--- App.tsx
+++ App.tsx
@@ -2,7 +2,10 @@
  * 企训通
  */
 
+import { startNetworkLogging } from 'react-native-network-logger';
 import AppNavigator from './src/navigation';
+
+startNetworkLogging();
 
 function App() {
   return <AppNavigator />;
